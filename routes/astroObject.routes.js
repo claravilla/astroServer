241d6826c2afd3594303c751b43b32f82177b9c2
Clara Villa <Clara.Villa@lego.com>
2022-03-02 15:22:31 +0000

    add get route for one object

--- routes/astroObject.routes.js
+++ routes/astroObject.routes.js
@@ -16,4 +16,22 @@ router.get("/", (req, res, next) => {
     });
 });
 
+//GET route for one object
+
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  AstroObject.findById(id)
+    .then((foundObject) => {
+      if (foundObject) {
+        console.log(foundObject);
+        res.status("200").json(foundObject);
+      } else {
+        res.status("200").json({ message: "Object not found" });
+      }
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
 module.exports = router;
