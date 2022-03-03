5452dc75a89c74d3412c8cd3a2caaa45f2af6c14
Clara Villa <Clara.Villa@lego.com>
2022-03-03 11:21:32 +0000

    login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,20 +5,20 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 
-//POST route
+//SIGN UP route
 
 router.post("/signup", (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!email || !password) {
     return res
-      .status("200")
+      .status(400)
       .json({ message: "Please provide all required fields" });
   }
 
   if (password.length < 6) {
     return res
-      .status("200")
+      .status(400)
       .json({ message: "Your password must be at least 6 characters" });
   }
 
@@ -28,7 +28,7 @@ router.post("/signup", (req, res, next) => {
   if (username) {
     User.findOne({ username: username }).then((foundUser) => {
       if (foundUser) {
-        return res.status("200").json({ message: "Username already exists" });
+        return res.status(400).json({ message: "Username already exists" });
       }
       return;
     });
@@ -47,23 +47,69 @@ router.post("/signup", (req, res, next) => {
         username: username,
         email: email,
         password: hashPassword,
-      }).then((newUser) => {
-        const { username, email, _id } = newUser;
-        const user = {
-          username,
-          email,
-          _id,
-        };
+      });
+    })
+    .then((newUser) => {
+      const { username, email, _id } = newUser;
+      const user = {
+        username,
+        email,
+        _id,
+      };
 
-        return res.status("200").json({
-          message: `user: ${user.username}, email: ${user.email} has been created`,
-        });
+      return res.status("200").json({
+        message: `user: ${user.username}, email: ${user.email} has been created`,
       });
     })
     .catch((error) => {
       console.log(error);
       next(error);
     });
+});
+
+//LOGIN route
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "Please provide all mandatory fields" });
+  }
+
+  //check if email exists
+
+  User.findOne({ email: email }).then((foundUser) => {
+    if (!foundUser) {
+      return res
+        .status(401)
+        .json({ message: "We couldn't log you in with those credentials" });
+    }
+
+    if (bcrypt.compareSync(password, foundUser.password)) {
+      const { username, email, _id } = foundUser;
+      //creating payload to be passed in the token
+      const payload = {
+        _id,
+        username,
+        email,
+      };
+
+      //creating the token
+
+      let token = jwt.sign(payload, process.env.TOKEN, {
+        algorithm: "HS256",
+        expiresIn: "1d",
+      });
+
+      console.log(token);
+
+      res.status(200).json({ authToken: token });
+    } else {
+      return res
+        .status(401)
+        .json({ message: "We couldn't log you in with those credentials" });
+    }
+  });
 });
 
 module.exports = router;
