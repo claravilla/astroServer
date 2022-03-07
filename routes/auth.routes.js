2a944b8a1bbdbfc0f76dbfd1e885a2932777a9fe
Clara Villa <Clara.Villa@lego.com>
2022-03-07 17:53:10 +0000

    refactor sign up route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,9 +8,8 @@ const { isAuthenticated } = require("../middleware/jwt.authenticated");
 
 //SIGN UP route
 
-router.post("/signup", (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   const { username, email, password } = req.body;
-  console.log("route file");
 
   if (!username || !email || !password) {
     res.status(400).json({ error: "Please fill all required fields" });
@@ -22,47 +21,89 @@ router.post("/signup", (req, res, next) => {
     return;
   }
 
-  User.findOne({ username: username })
-    .then((foundUser) => {
-      if (foundUser) {
-        res.status(400).json({ error: "Username already taken" });
-        return;
-      }
-      User.findOne({ email: email }).then((foundUser) => {
-        if (foundUser) {
-          res.status(400).json({ error: "Email already taken" });
-          return;
-        }
+  try {
+    let foundUser = await User.findOne({ username: username });
+    if (foundUser) {
+      res.status(400).json({ error: "Username already taken" });
+      return;
+    }
 
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hashedPassword = bcrypt.hashSync(password, salt);
-        return User.create({
-          username: username,
-          email: email,
-          password: hashedPassword,
-        }).then((newUser) => {
-          const { username, email, _id } = newUser;
-          const user = {
-            username,
-            email,
-            _id,
-          };
+    foundUser = await User.findOne({ email: email });
 
-          //creating the token
+    if (foundUser) {
+      res.status(400).json({ error: "Email already taken" });
+      return;
+    }
 
-          let token = jwt.sign(user, process.env.TOKEN, {
-            algorithm: "HS256",
-            expiresIn: "1d",
-          });
-
-          res.status(200).json({ user: user, authToken: token });
-        });
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({ error: "Internal server error" });
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
     });
+
+    const { username: newUsername, email: newEmail, _id: new_id } = newUser;
+    const user = {
+      username: newUsername,
+      email: newEmail,
+      _id: new_id,
+    };
+
+    //creating the token
+
+    let token = jwt.sign(user, process.env.TOKEN, {
+      algorithm: "HS256",
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ user: user, authToken: token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+
+  // User.findOne({ username: username })
+  //   .then((foundUser) => {
+  //     if (foundUser) {
+  //       res.status(400).json({ error: "Username already taken" });
+  //       return;
+  //     }
+  //     User.findOne({ email: email }).then((foundUser) => {
+  //       if (foundUser) {
+  //         res.status(400).json({ error: "Email already taken" });
+  //         return;
+  //       }
+
+  //       const salt = bcrypt.genSaltSync(saltRounds);
+  //       const hashedPassword = bcrypt.hashSync(password, salt);
+  //       return User.create({
+  //         username: username,
+  //         email: email,
+  //         password: hashedPassword,
+  //       }).then((newUser) => {
+  //         const { username, email, _id } = newUser;
+  //         const user = {
+  //           username,
+  //           email,
+  //           _id,
+  //         };
+
+  //         //creating the token
+
+  //         let token = jwt.sign(user, process.env.TOKEN, {
+  //           algorithm: "HS256",
+  //           expiresIn: "1d",
+  //         });
+
+  //         res.status(200).json({ user: user, authToken: token });
+  //       });
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     res.status(500).json({ error: "Internal server error" });
+  //   });
 });
 
 //LOGIN route
