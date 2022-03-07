80c896d0acf1e16f42a7e81a45b548205f72b133
Clara Villa <Clara.Villa@lego.com>
2022-03-07 13:49:14 +0000

    add get events route

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -54,4 +54,22 @@ router.post("/", (req, res, next) => {
     });
 });
 
+//GET EVENTS
+
+router.get("/", (req, res, next) => {
+  const { userId } = req.query;
+  console.log(userId);
+
+  Event.find({ userId: userId })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "Sorry, we couldn't retrieve your events, please try again",
+      });
+    });
+});
+
 module.exports = router;
