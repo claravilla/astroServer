e121ead989ab39f5501330f0c84ad31059a49cb8
Clara Villa <Clara.Villa@lego.com>
2022-03-08 11:10:47 +0000

    add protected route to events api

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+const { isAuthenticated } = require("./middleware/jwt.authenticated");
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes/index.routes");
@@ -30,7 +32,7 @@ app.use("/api/auth", authRoutes);
 //Event endpoints
 
 const EventRoutes = require("./routes/event.routes");
-app.use("/api/events", EventRoutes);
+app.use("/api/events", isAuthenticated, EventRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
